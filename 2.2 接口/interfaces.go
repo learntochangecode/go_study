7eafa891fe778c5279cae30b2b38deebb8999a68
ruixiang <1290457157@qq.com>
2024-09-02 10:06:06 +0800

    接口

---
+++ 2.2 接口/interfaces.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Abser interface {
+	Abs() float64
+}
+
+func main() {
+	var a Abser
+	f := MyFloat(-math.Sqrt2)
+	v := Vertex{3, 4}
+
+	a = f  // a MyFloat 实现了 Abser
+	a = &v // a *Vertex 实现了 Abser
+	// 下面一行会报错： cannot use v (type Vertex) as type Abser in assignment:
+	// 因为Vertex 没有实现 Abser
+	// a = v
+	fmt.Println(a.Abs())
+}
+
+// 类型通过实现一个接口的所有方法来实现该接口。
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
